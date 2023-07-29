c1a400c8324acab0e8c233acb06611a9cbc05770
makssozykin <mvsozykin@gmail.com>
2023-07-29 13:43:26 +0300

    first task module8 made

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createMarkup() { 
+    let markup = "";
+    for (let i = 0; i < galleryItems.length; i++) { 
+        markup += `<li class = "gallery__item">
+        <a class="gallery__link" href="${galleryItems[i].original}">
+        <img
+        class="gallery__image "
+        src="${galleryItems[i].preview}" 
+        alt="${galleryItems[i].description}"
+        target="_parent">
+        </a>
+        </li>`;
+    }
+    gallery.innerHTML = markup;
+}
+createMarkup();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        close: true,
+        closeText: '×',
+        nav: true,
+        navText: ['←', '→'],
+        overlay: true,
+        overlayOpacity: 0.5,
+        showCounter: false,
+        animationSlide: true,
+        widthRatio: 0.8,
+        heightRatio: 0.8,
+    });
