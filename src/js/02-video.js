988ee23c139c2f17af173f725c8a6e9ae151c0b3
makssozykin <mvsozykin@gmail.com>
2023-07-30 13:58:39 +0300

    second task module08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const LOCAL_STORAGE = "videoplayer-current-time";
+const player = new Player(iframe, {
+    loop: true,
+    fullscreen: true,
+    quality: '720p'
+});
+
+const onPlay = function (data) { 
+    localStorage.setItem(LOCAL_STORAGE, data.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE));
 
