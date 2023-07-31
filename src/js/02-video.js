867df38e4a49531a29bcf22cef209ddeb76a1582
makssozykin <mvsozykin@gmail.com>
2023-07-31 18:02:03 +0300

    fixed task 2 module 08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,16 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const LOCAL_STORAGE = "videoplayer-current-time";
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(data) { 
-    localStorage.setItem(LOCAL_STORAGE, data.seconds);
+    localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
 }
 
-const currentTime = localStorage.getItem(LOCAL_STORAGE) || 0;
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
 
 player.setCurrentTime(currentTime);
 
